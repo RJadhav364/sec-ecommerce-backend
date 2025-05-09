5dfe856bb2dea1a1913a3886492b8298c51ce486
Rohandesk <rohandesk372@gmail.com>
2025-05-09 19:24:18 +0530

    Test git flow

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -4,7 +4,7 @@ import productModel from "../models/ProductModel.js"
 const createNewProduct = async(req,res) => {
     try {
         console.log("HI")
-        res.status(200).send("HI")
+        res.status(200).send("HI, THIS API TO TEST GIT FLOW")
     } catch (error) {
         console.log(error)
     }
